ec183686498d49f0c04085f4e26af24ca5aa1143
changlei <changlei@graspyun.com>
2021-12-27 19:07:06 +0800

    feat: 初始提交

--- bin/graphical.dart
+++ bin/graphical.dart
@@ -2,13 +2,13 @@ import 'src/native.dart';
 
 void main(List<String> arguments) {
   print([
-    Graphical.cornerPath(100, 100, 8),
+    Graphical.cornerPath(100.0, 100.0, 8.0),
     Graphical.radians90,
     Graphical.radians180,
     Graphical.radians270,
     Graphical.radians360,
     Graphical.pi,
-    Graphical.fromSize(80, 80, 4, false),
-    Graphical.fromRadians(1, 4),
+    Graphical.fromSize(80.0, 80.0, 4.0, false),
+    Graphical.fromRadians(1.0, 4.0),
   ].join('\n'));
 }
