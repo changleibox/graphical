c257d445f26bc4cf3847049e83011324f3520d28
changlei <changlei@graspyun.com>
2021-12-27 19:44:48 +0800

    feat: 初始提交

--- bin/src/native.dart
+++ bin/src/native.dart
@@ -48,18 +48,27 @@ class CGOffset extends ffi.Struct {
   external double dx;
   @ffi.Double()
   external double dy;
+  @ffi.Double()
+  external double distance;
+  @ffi.Double()
+  external double direction;
 
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      other is CGOffset && runtimeType == other.runtimeType && dx == other.dx && dy == other.dy;
+      other is CGOffset &&
+          runtimeType == other.runtimeType &&
+          dx == other.dx &&
+          dy == other.dy &&
+          distance == other.distance &&
+          direction == other.direction;
 
   @override
-  int get hashCode => dx.hashCode ^ dy.hashCode;
+  int get hashCode => dx.hashCode ^ dy.hashCode ^ distance.hashCode ^ direction.hashCode;
 
   @override
   String toString() {
-    return 'CGOffset{dx: ${dx.toStringAsFixed(2)}, dy: ${dy.toStringAsFixed(2)}}';
+    return 'CGOffset{dx: ${dx.toStringAsFixed(4)}, dy: ${dy.toStringAsFixed(4)}, distance: ${distance.toStringAsFixed(4)}, direction: ${direction.toStringAsFixed(4)}}';
   }
 }
 
@@ -78,18 +87,39 @@ class CGSize extends ffi.Struct {
   external double width;
   @ffi.Double()
   external double height;
+  @ffi.Double()
+  external double direction;
+  @ffi.Double()
+  external double radians;
+  @ffi.Double()
+  external double distance;
+  @ffi.Double()
+  external double semiRadians;
 
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      other is CGSize && runtimeType == other.runtimeType && width == other.width && height == other.height;
+      other is CGSize &&
+          runtimeType == other.runtimeType &&
+          width == other.width &&
+          height == other.height &&
+          direction == other.direction &&
+          radians == other.radians &&
+          distance == other.distance &&
+          semiRadians == other.semiRadians;
 
   @override
-  int get hashCode => width.hashCode ^ height.hashCode;
+  int get hashCode =>
+      width.hashCode ^
+      height.hashCode ^
+      direction.hashCode ^
+      radians.hashCode ^
+      distance.hashCode ^
+      semiRadians.hashCode;
 
   @override
   String toString() {
-    return 'CGSize{width: ${width.toStringAsFixed(2)}, height: ${height.toStringAsFixed(2)}}';
+    return 'CGSize{width: ${width.toStringAsFixed(4)}, height: ${height.toStringAsFixed(4)}, direction: ${direction.toStringAsFixed(4)}, radians: ${radians.toStringAsFixed(4)}, distance: ${distance.toStringAsFixed(4)}, semiRadians: ${semiRadians.toStringAsFixed(4)}}';
   }
 }
 
@@ -114,6 +144,7 @@ class CGRect extends ffi.Struct {
   external double right;
   @ffi.Double()
   external double bottom;
+  external CGSize size;
 
   @override
   bool operator ==(Object other) =>
@@ -123,14 +154,15 @@ class CGRect extends ffi.Struct {
           left == other.left &&
           top == other.top &&
           right == other.right &&
-          bottom == other.bottom;
+          bottom == other.bottom &&
+          size == other.size;
 
   @override
-  int get hashCode => left.hashCode ^ top.hashCode ^ right.hashCode ^ bottom.hashCode;
+  int get hashCode => left.hashCode ^ top.hashCode ^ right.hashCode ^ bottom.hashCode ^ size.hashCode;
 
   @override
   String toString() {
-    return 'CGRect{left: ${left.toStringAsFixed(2)}, top: ${top.toStringAsFixed(2)}, right: ${right.toStringAsFixed(2)}, bottom: ${bottom.toStringAsFixed(2)}}';
+    return 'CGRect{left: ${left.toStringAsFixed(4)}, top: ${top.toStringAsFixed(4)}, right: ${right.toStringAsFixed(4)}, bottom: ${bottom.toStringAsFixed(4)}, size: $size}';
   }
 }
 
@@ -151,6 +183,15 @@ class CGIncircle extends ffi.Struct {
   external CGOffset middle;
   external CGOffset end;
   external CGOffset center;
+  @ffi.Double()
+  external double radius;
+  @ffi.Double()
+  external double radians;
+  @ffi.Double()
+  external double rotation;
+  external CGOffset vertex;
+  external CGRect circle;
+  external CGRect bounds;
 
   @override
   bool operator ==(Object other) =>
@@ -160,14 +201,30 @@ class CGIncircle extends ffi.Struct {
           begin == other.begin &&
           middle == other.middle &&
           end == other.end &&
-          center == other.center;
+          center == other.center &&
+          radius == other.radius &&
+          radians == other.radians &&
+          rotation == other.rotation &&
+          vertex == other.vertex &&
+          circle == other.circle &&
+          bounds == other.bounds;
 
   @override
-  int get hashCode => begin.hashCode ^ middle.hashCode ^ end.hashCode ^ center.hashCode;
+  int get hashCode =>
+      begin.hashCode ^
+      middle.hashCode ^
+      end.hashCode ^
+      center.hashCode ^
+      radius.hashCode ^
+      radians.hashCode ^
+      rotation.hashCode ^
+      vertex.hashCode ^
+      circle.hashCode ^
+      bounds.hashCode;
 
   @override
   String toString() {
-    return 'CGIncircle{begin: $begin, middle: $middle, end: $end, center: $center}';
+    return 'CGIncircle{begin: $begin, middle: $middle, end: $end, center: $center, radius: $radius, radians: $radians, rotation: $rotation, vertex: $vertex, circle: $circle, bounds: $bounds}';
   }
 }
 
